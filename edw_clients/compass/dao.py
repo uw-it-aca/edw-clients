11a30def14e1e3b8463f34195fe9c1e829925f76
Nicholas Falco <ncfalco@uw.edu>
2021-09-07 23:58:36 -0400

    Update compass query to concatenate major descriptions for student

--- edw_clients/compass/dao.py
+++ edw_clients/compass/dao.py
@@ -22,44 +22,79 @@ class CompassDAO(BaseDAO):
             raise ValueError(f"Exception occured parsing sis-term-id: {e}")
         yrq = "".join([str(year), str(quarter_num)])
 
-        query = (
-            f"""
-            SELECT DISTINCT TOP(250)
-                enr.StudentNumber,
-                stu1.uw_netid as UWNetID,
-                enr.StudentName,
-                enr.BirthDate,
-                enr.StudentEmail,
-                enr.ExternalEmail,
-                enr.LocalPhoneNumber,
-                enr.Gender,
-                enr.GPA,
-                enr.TotalCredits,
-                dm.MajorFullName,
-                enr.CampusDesc,
-                enr.ClassDesc,
-                enr.EnrollStatusCode
-            FROM EDWPresentation.sec.EnrolledStudent AS enr
-            LEFT JOIN UWSDBDataStore.sec.student_1 AS stu1 ON enr.SystemKey = stu1.system_key
-            LEFT JOIN EDWPresentation.sec.factStudentProgramEnrollment AS fspe ON fspe.StudentKeyId = enr.SystemKey
-            LEFT JOIN EDWPresentation.sec.dimMajor AS dm ON dm.MajorKeyId = fspe.MajorKeyId 
-            WHERE AcademicYrQtr = '{yrq}'
-            """  # noqa
-        )
-
+        filters_list = []
         if filters and filters.get("searchFilter"):
             filter_text = filters["searchFilter"]["filterText"]
             filter_type = filters["searchFilter"]["filterType"]
             if filter_type == "student-number":
-                query += f" AND enr.StudentNumber LIKE '%{filter_text}%'"
+                filters_list.append(
+                    f"AND StudentNumber LIKE '%{filter_text}%'")
             elif filter_type == "student-name":
-                query += (f" AND UPPER(enr.StudentName) LIKE "
-                          f"UPPER('%{filter_text}%')")
+                filters_list.append(
+                    f"AND UPPER(enr.StudentName) LIKE "
+                    f"UPPER('%{filter_text}%')")
             elif filter_type == "student-email":
-                query += (f" AND UPPER(enr.StudentEmail) LIKE "
-                          f"UPPER('%{filter_text}%')")
-        
-        query += " ORDER BY enr.StudentName"
+                filters_list.append(
+                    f"AND UPPER(enr.StudentEmail) LIKE "
+                    f"UPPER('%{filter_text}%')")
+
+        query = (
+            f"""
+            WITH student_info AS (
+                SELECT DISTINCT TOP(50)
+                    enr.StudentNumber,
+                    stu1.uw_netid as UWNetID,
+                    enr.StudentName,
+                    enr.BirthDate,
+                    enr.StudentEmail,
+                    enr.ExternalEmail,
+                    enr.LocalPhoneNumber,
+                    enr.Gender,
+                    enr.GPA,
+                    enr.TotalCredits,
+                    dm.MajorFullName,
+                    enr.CampusDesc,
+                    enr.ClassDesc,
+                    enr.EnrollStatusCode
+                FROM EDWPresentation.sec.EnrolledStudent AS enr
+                LEFT JOIN UWSDBDataStore.sec.student_1 AS stu1 ON enr.SystemKey = stu1.system_key
+                LEFT JOIN EDWPresentation.sec.factStudentProgramEnrollment AS fspe ON fspe.StudentKeyId = enr.SystemKey
+                LEFT JOIN EDWPresentation.sec.dimMajor AS dm ON dm.MajorKeyId = fspe.MajorKeyId 
+                WHERE AcademicYrQtr = '{yrq}' AND dm.MajorFullName IS NOT NULL {' '.join(filters_list)}
+            )
+            SELECT
+                StudentNumber,
+                UWNetID,
+                StudentName,
+                BirthDate,
+                StudentEmail,
+                ExternalEmail,
+                LocalPhoneNumber,
+                Gender,
+                GPA,
+                TotalCredits,
+                CampusDesc,
+                ClassDesc,
+                EnrollStatusCode,
+                STRING_AGG(MajorFullName, ', ') AS MajorDesc
+            FROM student_info
+            GROUP BY
+                StudentNumber,
+                UWNetID,
+                StudentName,
+                BirthDate,
+                StudentEmail,
+                ExternalEmail,
+                LocalPhoneNumber,
+                Gender,
+                GPA,
+                TotalCredits,
+                CampusDesc,
+                ClassDesc,
+                EnrollStatusCode
+            ORDER BY StudentName
+            """  # noqa
+        )
 
         conn = self.get_connection("EDWPresentation")
         enrolled_df = pd.read_sql(query, conn)
